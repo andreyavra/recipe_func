cfb20fe829957d29cd2f42fb07b027af71a345fa
Andrey Avramenko <andreyavra@gmail.com>
2020-01-11 01:52:11 +0000

    everything is great and it works perfectly

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 import re
 
 # GLOBALS
-RECIPE_ASK_PATTERN = re.compile(r"([Ww]hat's|[Gg]imme|[Gg]ive me|[Cc]huck me|[Cc]huck us) ((a )?recipe|(some )?food)( cuz)?(?P<args>.*)")
+RECIPE_ASK_PATTERN = re.compile(r"([Ww]hat's|[Gg]imme|[Gg]ive me|[Cc]huck me|[Cc]huck us) ((a )?recipe|(some )?food)( cuz)? ?(?P<args>.*)")
 RECIPE_CHOICE_PATTERN = re.compile(r"^(?P<choice>[0-9]+)$")
 STORAGE_LINK = 'https://store.ncss.cloud/'
 app = Flask(__name__)
@@ -54,17 +54,15 @@ def get_recipe_choice():
   author = args['author']
   room = args['room']
 
-  print("1work")
   m = RECIPE_CHOICE_PATTERN.match(message)
-  print("2work")
   if m is None:
+    text = "That isn't a valid response. Please try again. Select an option from 1 to 20"
     return jsonify({
       'author': 'Chef Bot',
       'room': args['room'],
-      'text': "That isn't a valid response. Please try again"
+      'text': text
     })
   else:
-    print("3work")
     choice = m.group('choice')
     recipe_id = get_item(f"syd-2/searched-recipes/{args['author']}/choices")[choice]
     print("4work")
@@ -73,7 +71,7 @@ def get_recipe_choice():
     return jsonify({
       'author': 'Chef Bot',
       'room': args['room'],
-      'text': "It worked!"
+      'text': f"Item {choice} selected"
     })
 
 
@@ -86,65 +84,63 @@ def get_recipe_choice():
 
 @app.route('/get_recipe', methods=['POST'])
 def get_recipe():
-  args = request.get_json()
+  resp = request.get_json()
 
-  message = args['text']
+  message = resp['text']
+
 
   m = RECIPE_ASK_PATTERN.match(message)
-  if m is not None:
-    print("yay")
+  if m is None:
+    print("INCORRECT AI YA")
 
-  '''
-  Different params:
-    - for {n} (hours), n minutes
-    -
-  '''
+  else:
+    args = m.group('args')
 
-  # BELOW IS THE TEST CODE FOR INGREDIENTS
-  ingredients = {
-    'cheese':{'quantity':2, 'expiration': '04042019'},
-    'chicken':{'quantity':1, 'expiration': '05052020'}
-  }
+    # BELOW IS THE TEST CODE FOR INGREDIENTS
+    ingredients = {
+      'cheese':{'quantity':2, 'expiration': '04042019'},
+      'chicken':{'quantity':1, 'expiration': '05052020'}
+    }
 
-  query = ""
-  for ingredient_name in ingredients:
-    curr_query = ingredient_name
-    quantity = ingredients[ingredient_name]['quantity']
-    if quantity > 1:
-      curr_query += "&number=" + str(quantity)
-    curr_query += ',+'
-    query += curr_query
+    query = ""
+    for ingredient_name in ingredients:
+      curr_query = ingredient_name
+      quantity = ingredients[ingredient_name]['quantity']
+      if quantity > 1:
+        curr_query += "&number=" + str(quantity)
+      curr_query += ',+'
+      query += curr_query
 
-  query = query.strip(',+')
+    query = query.strip(',+')
 
-  print(query)
+    print(query)
 
-  _params = {
-    'ingredients': query, 
-    'apiKey':'72aef675c4284f2ca32357d241c42284', 
-    'number':20
-  }
+    _params = {
+      'ingredients': query, 
+      'apiKey':'72aef675c4284f2ca32357d241c42284', 
+      'number':20
+    }
 
-  recipes = requests.get("https://api.spoonacular.com/recipes/findByIngredients", params=_params).json()
-    
-  user_recipes = {}
-  text = "Please pick an option by entering a number:<br>"
+    recipes = requests.get("https://api.spoonacular.com/recipes/findByIngredients", params=_params).json()
+      
+    user_recipes = {}
+    text = "Please pick an option by entering a number:<br>"
 
-  for i in range(len(recipes)):
-    title = recipes[i]['title']
-    recipe_id = recipes[i]['id']
-    user_recipes[i+1] = recipe_id
+    for i in range(len(recipes)):
+      title = recipes[i]['title']
+      recipe_id = recipes[i]['id']
+      user_recipes[i+1] = recipe_id
 
-    text += f'{i+1}: {title}<br>'
+      text += f'{i+1}: {title}<br>'
 
-  store_item(f"syd-2/searched-recipes/{args['author']}/choices", user_recipes)
-  text = text.strip('<br>')
+    store_item(f"syd-2/searched-recipes/{resp['author']}/choices", user_recipes)
+    text = text.strip('<br>')
 
-  return jsonify({
-    'author': 'Chef Bot',
-    'room': args['room'],
-    'text': text
-  })
+    return jsonify({
+      'author': 'Chef Bot',
+      'room': resp['room'],
+      'text': text
+    })
 
 
 
